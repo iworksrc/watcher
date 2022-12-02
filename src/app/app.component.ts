084bcb60b8529bc02312f84ac951e7fcbecf23bd
rke <imail.for.iwork@gmail.com>
2022-12-02 22:31:10 +0700

    itorum-theme plus css vars

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import {Component, HostListener, OnInit, ViewChild} from '@angular/core';
+import {MatSidenav} from "@angular/material/sidenav";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'watcher';
+  isAlterTheme = false;
+  @ViewChild('sidenav', { static: true }) sidenav!: MatSidenav;
+
+  opened = true;
+
+  ngOnInit(): void {
+    console.log(window.innerWidth);
+    this.trackWidth(window.innerWidth);
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize($event: any) {
+    this.trackWidth($event.target.innerWidth);
+  }
+
+  isBiggerScreen() {
+    const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    if (width < 768) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  private trackWidth(width: number) {
+    if (width < 768) {
+      this.sidenav.fixedTopGap = 64;
+      this.opened = false;
+    } else {
+      this.sidenav.fixedTopGap = 64;
+      this.opened = true;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from "@angular/material/button";
 import {MatCardModule} from "@angular/material/card";
+import {MatTabsModule} from "@angular/material/tabs";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatListModule} from "@angular/material/list";
 
 @NgModule({
   declarations: [
@@ -14,9 +19,15 @@ import {MatCardModule} from "@angular/material/card";
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    MatCardModule
+    MatCardModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
