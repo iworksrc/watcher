6b51af2b189e8492b379addd3cdbf65e20b63119
rke <imail.for.iwork@gmail.com>
2022-12-26 14:49:57 +0700

    some new fix ups

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,6 +18,10 @@ export class AppComponent implements OnInit {
   themeAccent = new Theme(124, 48, 47, 50, false, 'accent');
   themeWarn = new Theme(124, 48, 47, 50, false, 'warn');
 
+  themeAltPrimary = new Theme(206, 92.8, 43.5, 65, false, 'alt-primary');
+  themeAltAccent = new Theme(21, 88.5, 62.5, 65, false, 'alt-accent');
+  themeAltWarn = new Theme(353, 100, 50, 65, false, 'alt-warn');
+
   ngOnInit(): void {
     console.log(window.innerWidth);
     this.trackWidth(window.innerWidth);
